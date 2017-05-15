b116b5661463d2f152ee60d94fb594253039aac7
Cyndy Ishida <ishidacy@msu.edu>
2017-05-15 03:42:43 -0400

    practice scripts

---
+++ ishida_easy.py
@@ -0,0 +1,8 @@
+
+
+
+
+evens = len([ i for i in range(int(input())) if not i % 2] ) 
+print(evens)
+#assume the user will provide a numerical value in variable N. Write a solution that will count the even numbers starting from 0 that do not exceed value N. 
+#Zero is inclusive 
---
+++ ishida_hard.py
@@ -0,0 +1,3 @@
+
+
+#given a numerical value N, decrement the value by 1 if 
