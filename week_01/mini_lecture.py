df44455399f14baab43befa763b8a890da0050b4
Cyndy Ishida <ishidacy@msu.edu>
2017-05-15 03:43:32 -0400

    weeks

---
+++ week_01/mini_lecture.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 06:34:30 2016
+
+@author: Cyndy
+"""
+
+EEE = 2
+FFF = 1.2
+CCC = 0
+BBB = 10
+XXX = 15
+YYY = 0.1
+ZZZ = 5
+
+
+D = 1
+E = 1
+
+
+F = EEE // (FFF + CCC)
+G = FFF // 2 * 2
+
+
+
+
+A += 1
+
+#print("A: ", A, "\n")
+
+A *= BBB
+# short hand for A = A * BBB
+#print("A: ", A, "\n")
+
+
+
+
+D *= (BBB + 3)
+#print("D: ", D, "\n")
+
+
+
+
+
+E *= BBB + 3
+
+#print("E: ", E, "\n")
+
+
+
+B = int( XXX + 4.8 )
+
+#print("B: ", B, "\n")
+
+E = int( YYY - ZZZ )
+
+#print("E: ", E, )
